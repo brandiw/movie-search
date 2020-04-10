c6930ad93577462aa802de640d9f11c75c881b2b
brandiw <brandimichelle.0911@gmail.com>
2020-04-10 12:45:34 -0700

    Updated code

--- index.js
+++ index.js
@@ -1,12 +1,40 @@
+// Load the environment variables
+require('dotenv').config()
+
 // Require needed modules
 let express = require('express')
+let fetch = require('node-fetch')
 
 // Declare a new Express app
 let app = express()
 
+// Set the template language to EJS
+app.set('view engine', 'ejs')
+
 // Declare routes
 app.get('/', (req, res) => {
-    res.send('Home page stub')
+    res.render('home')
+})
+
+app.get('/search', (req, res) => {
+    let page = req.query.page || 1
+    let url = `http://www.omdbapi.com/?s=${req.query.query}&apikey=${process.env.OMDB_API_KEY}&page=${page}`
+
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+        res.render('results', { 
+            results: data.Search, 
+            query: req.query.query, 
+            page: parseInt(page) 
+        })
+    })
+    .catch(err => {
+        console.log('AN error!', err)
+        res.send('Error - check logs!')
+    })
+
 })
 
 // Pick a port for it to listen on
